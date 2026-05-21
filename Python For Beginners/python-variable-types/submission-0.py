8925f64c29e02055bc74a22a0e0739f905aef334
Tarang Pande <pandet.tp@gmail.com>
2026-05-20 20:45:28 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 67
+float_type = 6.7
+bool_type = True
+string_type = "67"
+list_type = [6,7]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
